e47097a14730ea3d92721f32ac47b1001f5b7409
coder-aastha <aasthadahal312@gmail.com>
2024-12-24 13:21:55 +0545

    Responsive Home Page Completed

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import nestfinder_logo from "../../../public/nestfinder_logo.png";
+import "./navbar.scss";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src={nestfinder_logo} alt="" width="80px" height="80px" />
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/">Sign In</a>
+        <a href="/" className="register">
+          Sign Up
+        </a>
+        <div className="menuIcon">
+          <img src="/menu.png" alt="" onClick={() => setOpen((prev) => !prev)} />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign In</a>
+          <a href="/">Sign Up</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/routes/homePage/homePage.jsx
@@ -0,0 +1,42 @@
+import SearchBar from "../../components/searchBar/SearchBar";
+import "./homePage.scss";
+
+const HomePage = () => {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">
+            Discover Your Dream Home & Find Your Perfect Place
+          </h1>
+          <p>
+            Browse through a wide selection of properties to find the perfect
+            match for your lifestyle. Whether you&apos;re looking to buy or
+            rent, discover your ideal home with ease and start a new chapter
+            today.
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experiences</h2>
+            </div>
+            <div className="box">
+              <h1>200+</h1>
+              <h2>Awards Gained</h2>
+            </div>
+            <div className="box">
+              <h1>1200+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
