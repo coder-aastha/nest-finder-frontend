2c6d3ce7f15734e8abb695c3b3f9af89952fcdeb
coder-aastha <aasthadahal312@gmail.com>
2025-01-07 14:29:17 +0545

    Created List.jsx

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,15 +1,14 @@
-import "./list.scss"
-import Card from "../card/Card"
-import {listData} from "../../lib/dummydata"
+import Card from "../card/Card";
+import "./list.scss";
 
-const List = () => {
+const List = ({ posts }) => {
   return (
     <div className="list">
-      {listData.map(item => (
+      {posts.map((item) => (
         <Card key={item.id} item={item} />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
