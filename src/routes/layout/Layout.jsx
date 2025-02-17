1a4ae6ecb5020d13c3cd1f557d05a63ef3287879
coder-aastha <aasthadahal312@gmail.com>
2025-02-17 10:07:54 +0545

    modified layout

--- src/routes/layout/Layout.jsx
+++ src/routes/layout/Layout.jsx
@@ -1,6 +1,8 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import Navbar from "../../components/navbar/Navbar";
 import "./layout.scss";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
 
 const Layout = () => {
   return (
@@ -15,4 +17,23 @@ const Layout = () => {
   );
 };
 
-export default Layout;
+const RequireAuth = () => {
+
+  const {currentUser} = useContext(AuthContext);
+  if(!currentUser){
+    return <Navigate to="/login" />
+  }
+
+  return (
+     currentUser && (<div className="layout">
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="content">
+          <Outlet />
+        </div>
+    </div>)
+  );
+};
+
+export {Layout, RequireAuth};
